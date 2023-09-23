8ccbbf9cdbc7ace70b4a74a5980d77815694d8f2
audreydandurand <112189528+audreydandurand@users.noreply.github.com>
2023-09-22 21:29:23 -0400

    ajout_dossier

---
+++ script.js
@@ -0,0 +1,28 @@
+let chapters = {
+    debut : {
+        titre: "6.5 gallons au compteur", 
+        description: "dhfgdhf", 
+        image: "fdg",
+        boutons: []
+    },
+
+    forest : {
+        titre: "6.5 gallons au compteur", 
+        description: "dhfgdhf", 
+        image: "fdg",
+        boutons: []
+    },
+}
+
+function goToChapter(cle){
+
+    var chapitre = chapters(cle);
+    chapitre.titre
+
+}
+//
+
+
+
+
+goToChapter('forest')
