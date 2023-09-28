98ffcb6d678d849ae0de962a3bf24c520f30615b
audreydandurand <112189528+audreydandurand@users.noreply.github.com>
2023-09-27 21:58:50 -0400

    correction_orthographe

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 let chapters = {
     debut : {
         titre: "Aventure de rêve", 
-        description: "En après-midi, tu décides d'aller faire un tour d'hélicoptère avec ton ami qui vient de terminer sa formation en tant que pilote privé. Vous vous êtes promené au dessus de la mer et vous avez parcouru une bonne distance. Votre ami remarque donc qu'il vous reste 6,5 gallons d'essence. Étant assez loin des berges, vous ne savez pas qu'elle est la meilleure décision à prendre.", 
+        description: "En après-midi, tu décides d'aller faire un tour d'hélicoptère avec ton ami qui vient de terminer sa formation en tant que pilote privé. Vous vous êtes promené au-dessus de la mer et vous avez parcouru une bonne distance. Votre ami remarque donc qu'il vous reste 6,5 gallons d'essence. Étant assez loin des berges, vous ne savez pas qu'elle est la meilleure décision à prendre.", 
         image: "./assets/image/helicoptere.jpg",
         boutons: [
-            {titre: 'Atterissage sur un banc de sable', destination: 'atterissage'},
+            {titre: 'Atterissage sur un banc de sable', destination: 'atterrissage'},
             {titre: 'Continuer d\'avancer vers la rive la plus proche', destination: 'rive'}
         ]
     },
 
-    atterissage : {
-        titre: "Atterissage forcé", 
-        description: "Vous avez réalisé que vous rendre à votre point de départ ou de l'autre côté de la rive était impossible. Atterrir sur le banc de sable que vous voyiez du haut de l'hélicoptère était la meilleure option. Que sera la meilleure action à faire suite à votre atterissage forcé?  ", 
-        image: './assets/image/atterissage.jpg',
+    atterrissage : {
+        titre: "Atterrissage forcé", 
+        description: "Vous avez réalisé que vous rendre à votre point de départ ou de l'autre côté de la rive était impossible. Atterrir sur le banc de sable que vous voyiez du haut de l'hélicoptère était la meilleure option. Que sera la meilleure action à faire suite à votre atterrissage forcé?  ", 
+        image: './assets/image/atterrissage.jpg',
         boutons: [
             {titre: 'Partir à la recherche des rames', destination: 'rame'},
             {titre: 'Partir à la recherche du bateau de sauvetage', destination: 'bateau'},
@@ -23,7 +23,7 @@ let chapters = {
     rive : {
         titre: "Vous vous écraser", 
         description: "Croyant vous rendre à la rive, vous avez dépensé tout votre carburant sans pouvoir atterrir sain et sauf. Vous vous êtes écrasé en plein milieu de la mer et vous n'avez pas survécu.", 
-        image: "fdg",
+        image: "./assets/image/rive.jpg",
         boutons: [
             {titre: 'Retour au début', destination: 'debut'}
         ]
@@ -32,7 +32,7 @@ let chapters = {
     rame : {
         titre: "À la recherche des équipements de survie", 
         description: "Votre ami vous explique que les rames ne sont pas rangées au même endroit que le bateau de sauvetage. Pendant qu'il cherche le bateau de sauvetage, vous avez décidé de regarder dans les compartiments de l'hélicoptère pour trouver les rames dans le but d'utiliser le bateau de sauvetage.  ", 
-        image: "fdg",
+        image: "./assets/image/rame.jpg",
         boutons: [
             {titre: 'Utiliser le bateau de sauvetage', destination: 'bateau'},
         ]
@@ -40,7 +40,7 @@ let chapters = {
 
     bateau : {
         titre: "Le bateau de sauvetage ", 
-        description: "Après avoir atteri sur le banc de sable, vous commencez à chercher avec votre ami le bateau de sauvetage rangé dans l'hélicoptère. Dès que vous le trouvez, vous embarquez tous les deux dans le bateau prêt à regagner la rive. Vous vous éloignez assez rapidement du banc de sable à cause de la marée. Vous demandez à votre ami de vous donner une rame pour pagayer vers la rive.", 
+        description: "Après avoir atterri sur le banc de sable, vous commencez à chercher avec votre ami le bateau de sauvetage rangé dans l'hélicoptère. Dès que vous le trouvez, vous embarquez tous les deux dans le bateau prêt à regagner la rive. Vous vous éloignez assez rapidement du banc de sable à cause de la marée. Vous demandez à votre ami de vous donner une rame pour pagayer vers la rive.", 
         image: "./assets/image/bateau_sauvetage.jpg",
         boutons: [
             {titre: 'Avoir les rames à porter de main', destination: 'helicoptere'},
@@ -51,9 +51,9 @@ let chapters = {
     radio : {
         titre: "Appel à l'aide", 
         description: "Vous avez décidé de demander de l'aide à la garde côtière en utilisant la radio satellite. Par chance, ils vous répondent rapidement et ils viendront vous chercher. Vous ne savez pas exactement dans combien de temps, ils vous retrouveront. Les heures avancent et vous êtes toujours là. La marée, elle, elle augmente beaucoup plus vite. Vous ne croyez pas pouvoir rester assis dans le sable sec très longtemps encore. ", 
-        image: "fdg",
+        image: "./assets/image/radio.jpg",
         boutons: [
-            {titre: 'Aller sur le toit de l\'hélicoptere', destination: 'helicoptere'},
+            {titre: 'Aller sur le toit de l\'hélicoptère', destination: 'helicoptere'},
             {titre: 'Embarquer dans le bateau de sauvetage', destination: 'helicoptere'}
         ]
     },
